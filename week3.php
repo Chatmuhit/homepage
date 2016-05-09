a6ee7fd94d434d12af27b8db71e8fcbb8dbba1f3
Chatmuhit <austin.ascura@gmail.com>
2016-05-09 03:52:39 -0600

    modified for some basic variable passing in week3

--- week3.php
+++ week3.php
@@ -1,3 +1,25 @@
 <!DOCTYPE html>
 <?php
+$isGamer = $_POST["isGamer"];
+$ownsEquipment = $_POST["ownsEquipment"];
+
 ?>
+
+<html>
+<head></head>
+<body>
+<?php
+switch($isGamer){
+	case true:
+	echo "True";
+	break;
+	case false:
+	echo "False";
+	break;
+	default:
+	echo "Else";
+	break;
+	}
+?>
+</body>
+</html>
