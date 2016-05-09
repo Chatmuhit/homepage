4b452df69bc8e5978dd5462ca1285b71113bc6d9
Chatmuhit <austin.ascura@gmail.com>
2016-05-09 04:27:06 -0600

    Checking differences in variable comparison for multiple variables

--- week3.php
+++ week3.php
@@ -2,7 +2,15 @@
 <?php
 $isGamer = $_POST["isGamer"];
 $ownsEquipment = $_POST["ownsEquipment"];
-
+$ownPC = $_POST["ownPC"];
+$ownMac = $_POST["ownMac"];
+$ownMobile = $_POST["ownMobile"];
+$ownPS4 = $_POST["ownPS4"];
+$ownVita = $_POST["ownVita"];
+$ownXbone = $_POST["ownXbone"];
+$ownWiiU = $_POST["ownWiiU"];
+$own3DS = $_POST["own3DS"];
+$numHoursPlayed = $_POST["numHoursPlayed"];
 ?>
 
 <html>
@@ -12,10 +20,110 @@ $ownsEquipment = $_POST["ownsEquipment"];
 
 switch($isGamer){
 	case 0:
-		echo "False";
+		echo "Is not Gamer\n";
 		break;
 	case 1:
-		echo "True";
+		echo "Is Gamer\n";
+		break;
+	default:
+		echo "Else";
+		break;
+}
+
+switch($ownsEquipment){
+	case 0:
+		echo "Does not own\n";
+		break;
+	case 1:
+		echo "Owns\n";
+		break;
+	default:
+		echo "Else";
+		break;
+}
+switch($ownPC){
+	case 0:
+		echo "Doesn't own PC\n";
+		break;
+	case 1:
+		echo "Owns PC\n";
+		break;
+	default:
+		echo "Else";
+		break;
+}
+switch($ownMac){
+	case 0:
+		echo "Doesn't own Mac\n";
+		break;
+	case 1:
+		echo "Owns Mac\n";
+		break;
+	default:
+		echo "Else";
+		break;
+}
+switch($ownMobile){
+	case 0:
+		echo "Doesn't own Mobile\n";
+		break;
+	case 1:
+		echo "Owns Mobile\n";
+		break;
+	default:
+		echo "Else";
+		break;
+}
+switch($ownPS4){
+	case 0:
+		echo "Doesn't own PS4\n";
+		break;
+	case 1:
+		echo "Owns PS4\n";
+		break;
+	default:
+		echo "Else";
+		break;
+}
+switch($ownVita){
+	case 0:
+		echo "Doesn't own Vita\n";
+		break;
+	case 1:
+		echo "Owns Vita\n";
+		break;
+	default:
+		echo "Else";
+		break;
+}
+switch($ownXbone){
+	case 0:
+		echo "Doesn't own Xbone\n";
+		break;
+	case 1:
+		echo "Owns Xbone\n";
+		break;
+	default:
+		echo "Else";
+		break;
+}
+switch($ownWiiU){
+	case 0:
+		echo "Doesn't own Wii U\n";
+		break;
+	case 1:
+		echo "Owns Wii U\n";
+		break;
+	default:
+		echo "Else";
+		break;
+}
+switch($own3DS){
+	case 0:
+		echo "Doesn't own 3DS\n";
+		break;
+	case 1:
+		echo "Owns 3DS\n";
 		break;
 	default:
 		echo "Else";
